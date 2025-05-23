deada8f3215a68ad3283ee8b3a4fab5f1cdc1d11
Taufik Ma'ruf <taufik.maruf123@gmail.com>
2025-05-23 20:13:15 +0700

    Create 1 Variable.py

---
+++ 1 Variable.py
@@ -0,0 +1,4 @@
+age = 25 # Melakukan assigment pada variabel 'age' dengan nilai '25'
+
+print ('Hello, my name is Taufik Ma'ruf') # Menampilkan Teks pada Console
+print (age) # Akan menampilkan '25' pada Console
