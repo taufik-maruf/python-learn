756731e362f7ab884dde3e2e4bf91836cd1fe5a2
Taufik Ma'ruf <taufik.maruf123@gmail.com>
2025-05-23 20:36:01 +0700

    Create 2 Numbers.py

---
+++ 2 Numbers.py
@@ -0,0 +1,9 @@
+day = 23 # Melakukan assign pada variabel day dengan nilai '23', nilai merupakan bilangan integer (bilangan bulat)
+temp = -36 # Integer dapat berupa bilangan negatif
+
+height = 174.3 # Float merupakan bilangan dengan koma (Desimal)
+
+# Operasi
+print(3 + 6) # Akan menghasilkan 9 pada Console
+print(day+3) # Akan menghasilkan 26 pada Console
+print(height * 2) # Akan menghasilkan 348.6 pada Console
