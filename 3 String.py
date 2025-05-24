be6eb916c635a791c2f088df0c62fc056d71ef39
Taufik Ma'ruf <taufik.maruf123@gmail.com>
2025-05-24 10:27:38 +0700

    Create 3 String.py

---
+++ 3 String.py
@@ -0,0 +1,3 @@
+shirt = "Blue" # String = Teks, dapat berupa huruf, angka, ataupun simbol. Penulisan harus diawali dan diakhiri dengan tanda petik
+
+print (shirt) # Akan menghasilkan "Blue" pada Console
