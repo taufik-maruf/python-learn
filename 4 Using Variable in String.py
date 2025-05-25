9046fa85abb73def9ef9f3caf433bc4fb1da8f5c
Taufik Ma'ruf <taufik.maruf123@gmail.com>
2025-05-25 09:05:44 +0700

    Create 4 Using Variable in String.py

---
+++ 4 Using Variable in String.py
@@ -0,0 +1,7 @@
+day = 25
+month = 'May'
+temp = 36.8
+
+print(f'Today is {month} {day} 2025, and {temp} celcius') # Menambahkan 'f' sebelum string pada fungsi print agar variabel dapat dimasukan dengan kurung kurawal {}
+
+# Maka setiap nilai dari variabel month, day, dan temp berubah. hasil print juga akan berubah
