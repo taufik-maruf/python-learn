04a1d7bcb5b925a07f7b325f7f98d452b4ee5c1f
Taufik Ma'ruf <taufik.maruf123@gmail.com>
2025-05-25 09:17:12 +0700

    Create 5 IF and Boolean.py

---
+++ 5 IF and Boolean.py
@@ -0,0 +1,13 @@
+age = 25         # Integer
+Name = 'Taufik'  # String
+Height = 174.5   # Float
+
+light_on = True  # Boolean (True/False) tanpa menggunakan tanda petik
+tv_on = 'True'   # String, bukan termasuk Boolean karena menggunakan tanda petik
+
+# IF Statement, program akan dieksekusi jika memenuhi kondisi (Benar/True)
+if light_on:
+  print('The Light is on')  # Setiap command dibawah if harus diberi jarak, yang berarti program akan berjalan tergantung kondisi sebelumnya
+print('Program Complete')   # Jika tidak diberi jarak, program akan tetap dieksekusi tanpa menunggu info dari IF
+
+# Program yang eksekusinya bergantung pada IF adalah yang diberikan jarak 1 tab
