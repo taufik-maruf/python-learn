a789ec001441d39c867444b4c3825ffdd7a9e798
Taufik Ma'ruf <taufik.maruf123@gmail.com>
2025-05-25 09:55:40 +0700

    Create 6 ELSE and Comparison.py

---
+++ 6 ELSE and Comparison.py
@@ -0,0 +1,15 @@
+# ELSE --> jika program tidak sesuai maka akan dijalankan
+
+light_on = True
+if light_on:
+  print('It\'s  bright here')      # Jika True akan dieksekusi
+else:
+  print('Oh no, it\'s dark here')  # Jika False akan dieksekusi
+
+# Comparison
+
+age = 25
+if age >= 18:      # Jika variabel 'age' lebih dari sama dengan 18 (Benar/True)
+  print('adult')   # Program akan dieksekusi
+else:
+  print('child')   # Jika kurang dari 18 (False) Program akan dieksekusi
