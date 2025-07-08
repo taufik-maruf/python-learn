4fe755912693bfc2058997c1a39cc5e6c270c38f
Taufik Ma'ruf <taufik.maruf123@gmail.com>
2025-07-08 14:42:44 +0700

    Create List dan Tuple.py

---
+++ List dan Tuple.py
@@ -0,0 +1,9 @@
+# Mengakses List dan Tuple
+bulan_pembelian = ('Januari','Febuari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober', 'November','Desember')
+
+print(bulan_pembelian[0])	# Menampilkan Bulan Januari
+print(bulan_pembelian[5])	# Menampilkan Bulan Juni
+
+# Dapat juga menggunakan indeks negatif
+print(bulan_pembelian[-1])	# Menampilkan Bulan Desember
+print(bulan_pembelian[-2])	# Menampilkan Bulan November
