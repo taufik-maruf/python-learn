0aa8ccf1ba333d57e5e6618b02a8cad28292649a
Taufik Ma'ruf <taufik.maruf123@gmail.com>
2025-07-09 10:43:27 +0700

    Create Menggabungkan List.py

---
+++ Menggabungkan List.py
@@ -0,0 +1,5 @@
+# Penggabungan 2 atau lebih List/Tuple
+list_makanan = ['Gado-Gado','Ayam Goreng','Rendang']
+list_minuman = ['Es Teh', 'Es Jeruk', 'Es Campur']
+list_menu = list_makanan + list_minuman
+print(list_menu)
